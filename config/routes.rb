860d2bc5131c81563d58982ac35e638d29184987
Olli Karppinen <olli.karppinen@relex.fi>
2016-10-10 23:43:56 +0300

    Removes unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :votes, only: [:index, :new, :create, :show]
-  resources :voting_ends, only: [:new, :create]
-  resources :voting_starts, only: [:new, :create]
+  resources :voting_ends, only: [:create]
+  resources :voting_starts, only: [:create]
   resources :voting_forms, only: [:show]
-  resources :voting_decisions, only: [:new, :create]
+  resources :voting_decisions, only: [:create]
 end
