fed18f7f6a71e3819cfbbb02fd97941adb2025cf
Olli Karppinen <olli.karppinen@relex.fi>
2016-10-11 20:41:19 +0300

    Adds notice alert boxes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -30,7 +30,7 @@ class VotesController < ApplicationController
 
     respond_to do |format|
       if @vote.save
-        format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
+        format.html { redirect_to @vote, notice: 'Voting was successfully created.' }
         format.json { render :show, status: :created, location: @vote }
       else
         format.html { render :new }
@@ -43,10 +43,10 @@ class VotesController < ApplicationController
     @uuid = params[:uuid]
     if @uuid == @vote.start_uuid && !@vote.has_started?
       @vote.voting_start = VotingStart.create(vote_id: @vote.id)
-      @notice = 'Vote was successfully started.'
+      @notice = 'Voting was successfully started.'
     elsif @uuid == @vote.end_uuid && !@vote.has_ended?
       @vote.voting_end = VotingEnd.create(vote_id: @vote.id)
-      @notice = 'Vote was successfully ended.'
+      @notice = 'Voting was successfully ended.'
     end
     respond_to do |format|
       format.html { redirect_to @vote, notice: @notice }
